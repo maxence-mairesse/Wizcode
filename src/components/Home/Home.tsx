8741df1904ae110f68ec84e01d6bc9fabec06355
Moscato1 <mairessemax@live.fr>
2024-10-05 16:26:14 +0200

    Logo page Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,18 @@
+import './Home.scss';
+
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <div className="Home">
+      <div className="presentation">
+        <img src="../../public/logo.png" alt="logo site" />{' '}
+        <h3>Maxence Mairesse</h3>
+        <h4>Développeur Fullstack Freelance</h4>
+      </div>
+      <div className="accroche">
+        <h2>Ensemble</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

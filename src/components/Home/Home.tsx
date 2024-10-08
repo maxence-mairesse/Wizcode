7eac2a21d1f8c51047fb5200a47ed818d51d586c
Moscato1 <mairessemax@live.fr>
2024-10-08 17:34:40 +0200

    texte accroche

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,51 @@ const Home = () => {
         <h4>Développeur Fullstack Freelance</h4>
       </div>
       <div className="accroche">
-        <h2>Ensemble</h2>
+        <div className="title">
+          <hr />
+          <h3>Ensemble</h3>
+          <hr />
+        </div>
+        <div>
+          <h2>Créons un produit innovant </h2>
+        </div>
+        <div className="adjectif">
+          <div>
+            <ul>
+              <li>
+                <i className="fa-solid fa-medal" />
+              </li>
+              <li>Qualité</li>
+              <li>
+                Faites passer votre projet web à un niveau supérieur avec une
+                solution technologique de pointe.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <i className="fa-solid fa-bullseye" />
+              </li>
+              <li>Objectif</li>
+              <li>
+                On conçoit et développe des solutions adaptées à vos besoins.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <i className="fa-solid fa-message" />
+              </li>
+              <li>Communication</li>
+              <li>
+                100 % à distance, on communique et on organise les objectifs en
+                utilisant la méthode Agile.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
